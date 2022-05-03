d8a5cf96d1fc3c9b2d98709133d1389a3069253a
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-05-03 20:21:24 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { ComicsComponent } from './comics/comics.component';
+import { EventsComponent } from './events/events.component';
+import { SeriesComponent } from './series/series.component';
+import { StoriesComponent } from './stories/stories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    ComicsComponent,
+    EventsComponent,
+    SeriesComponent,
+    StoriesComponent
   ],
   imports: [
     BrowserModule,
