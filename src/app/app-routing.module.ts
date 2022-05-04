2e08edbc7dadbc90f841f10d16368727a16e9f3b
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-05-03 21:46:08 -0300

    configuration routing module path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { ComicsComponent } from './comics/comics.component';
+import { EventsComponent } from './events/events.component';
+import { SeriesComponent } from './series/series.component';
+import { StoriesComponent } from './stories/stories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent },
+  {path: 'characters', component: CharactersComponent},
+  {path: 'comics', component: ComicsComponent},
+  {path: 'events', component: EventsComponent },
+  {path: 'series', component: SeriesComponent},
+  {path: 'stories', component: StoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
