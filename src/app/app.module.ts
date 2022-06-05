c58d8c8a54f3290e7a6b3298436fb71c8c961ef8
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-06-05 15:31:49 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharactersComponent } from './characters/characters.component';
 import { ComicsComponent } from './comics/comics.component';
-import { EventsComponent } from './events/events.component';
+
 import { SeriesComponent } from './series/series.component';
 import { StoriesComponent } from './stories/stories.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -19,7 +19,6 @@ import { CharacterComponent } from './characters/character/character.component';
     AppComponent,
     CharactersComponent,
     ComicsComponent,
-    EventsComponent,
     SeriesComponent,
     StoriesComponent,
     NavbarComponent,
